f2763a931eea655b71d4072d59659c534214272c
snow-oc <snow7349.oc@gmail.com>
2025-07-28 00:40:06 +0900

    基本的な変数とデータ型の確認方法

---
+++ code/01.py
@@ -0,0 +1,17 @@
+age = 20 # int
+height = 170.5 # float
+name = "yamada" # str
+is_student = True # bool
+
+print("年齢:", age)
+print("身長:", height)
+print("名前:", name)
+print("学生ですか？", is_student)
+
+print(type(age)) # <class 'int'>
+print(type(height)) # <class 'float'>
+print(type(name)) # <class 'str'>
+print(type(is_student)) # <class 'bool'>
+
+note_price = 250
+print("ノートの4冊の金額は:", note_price * 4, "円です")
