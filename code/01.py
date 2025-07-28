9c9796ba0c800ef0404862335cb9d6d8cf7a25d3
snow-oc <snow7349.oc@gmail.com>
2025-07-29 00:08:48 +0900

    f文字列の勉強

--- code/01.py
+++ code/01.py
@@ -1,6 +1,6 @@
 age = 20 # int
 height = 170.5 # float
-name = "yamada" # str
+name = "山田" # str
 is_student = True # bool
 
 print("年齢:", age)
@@ -15,3 +15,10 @@ print(type(is_student)) # <class 'bool'>
 
 note_price = 250
 print("ノートの4冊の金額は:", note_price * 4, "円です")
+
+# f文字列
+print(f"{name}さんの身長は{height}cmです。")
+# 大文字、小文字
+fruit1 = "Apple"
+print(fruit1.upper())
+print(fruit1.lower())
